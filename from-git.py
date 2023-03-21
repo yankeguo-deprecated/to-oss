7c2934e500cf6b3ee45148f7492767b161aaaf55
Guo Y.K <hi@guoyk.net>
2023-03-21 14:05:10 +0800

    fix filename

--- from-git.py
+++ from-git.py
@@ -22,7 +22,7 @@ def main():
     u: urllib3.util.Url = urllib3.util.parse_url(git_url)
 
     key = "repos/" + u.hostname + "/" + \
-          u.path.removesuffix(".git").lower().replace('/', '--') + "-" + \
+          u.path.removeprefix("/").removesuffix(".git").lower().replace('/', '--') + "-" + \
           datetime.datetime.now().strftime('%Y%m%d-%H%M%S') + \
           ".tar"
 
